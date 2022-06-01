4e4aa5b31af75934cb22a057c45ee3c7300abe9a
Krunali <krunalilole29@gmail.com>
2022-06-01 17:17:39 +0530

    Adding UC-2 to use service layer to get simple greeting

--- greetingapp/greetingapp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ greetingapp/greetingapp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,21 +1,49 @@
 package com.bridgelabz.greetingapp.controller;
 
 import com.bridgelabz.greetingapp.model.Greeting;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.bridgelabz.greetingapp.model.User;
+import com.bridgelabz.greetingapp.service.IGreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
-    private static final String template = "Hello , %s!";
-    private final AtomicLong counter = new AtomicLong();
+     private static final String template = "Hello , %s!";
+     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/greeting")
-
-    public Greeting greeting(@RequestParam(value = "name",defaultValue = "World")String name)
-    {
-        return new Greeting(counter.incrementAndGet(), String.format(template,name));
+    @Autowired
+    private IGreetingService greetingService;
+/**
+//     * @Func :Returning JSON Object
+//     * @URL: http://localhost:8080/home  -Postman-GET
+//     * @return: hello
+     */
+    @GetMapping(value={" ", "/" , "/home"})
+    public Greeting greeting(@RequestParam(value = "firstName", defaultValue = "World") String firstName,
+                             @RequestParam(value = "lastName", defaultValue = "") String lastName) {
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        return greetingService.addGreeting(user);
     }
+    @GetMapping("/path/{id}")
+    public Greeting getElementById(@PathVariable Long id){
+        return greetingService.getGreetingById(id);
+    }
+    @GetMapping("/all")
+    public List<Greeting> getAll(){
+        return greetingService.getAll();
+    }
+    @PutMapping("/edit/{id}")
+    public Greeting editGreetingById(@PathVariable long id, @RequestParam String name){
+        return greetingService.editGreetingById(id, name);
+    }
+    @DeleteMapping("/delete/{id}")
+    public List<Greeting> deleteGreetingById(@PathVariable Long id){
+        return greetingService.deleteGreetingById(id);
+    }
+
 }
